2cb0be618f36f35dd0197ff966700e8ed22d16ee
benjaminmao123 <43651347+benjaminmao123@users.noreply.github.com>
2019-02-25 13:17:18 -0800

    Update CommandReceiver.cpp

--- Assignments/Assignment_3/src/CommandReceiver.cpp
+++ Assignments/Assignment_3/src/CommandReceiver.cpp
@@ -120,96 +120,18 @@ bool CommandReceiver::Test(std::vector<std::string> &ArgumentList) const
 {
 	struct stat buf;
 	
-	if (ArgumentList[0] == "test")
+	if (ArgumentList.size() == 1)
 	{
-		if (ArgumentList.size() == 1)
-		{
-			std::cout << "Shell: syntax error: test: missing path name\n";
-			return false;
-		}
-		else if (ArgumentList.size() == 2 && (ArgumentList[1] == "-e" || ArgumentList[1] == "-f" || ArgumentList[1] == "-d"))
-		{
-			std::cout << "Shell: syntax error: test: missing path name\n";
-			return false;
-		}
-		else
-		{
-			if (ArgumentList[1] == "-e")
-			{
-				std::string path = ArgumentList[2];
-			
-				if (!stat(path.c_str(), &buf))
-				{
-					std::cout << "(True)\n";
-					return true;
-				}
-				
-				std::cout << "(False)\n";
-				return false;
-			}
-			else if (ArgumentList[1] == "-f")
-			{
-				std::string path = ArgumentList[2];
-				
-				if (stat(path.c_str(), &buf))
-				{
-					return false;
-				}
-				
-				if (S_ISREG(buf.st_mode))
-				{
-					std::cout << "(True)\n";
-					return true;
-				}
-				
-				std::cout << "(False)\n";
-				return false;
-			}
-			else if (ArgumentList[1] == "-d")
-			{
-				std::string path = ArgumentList[2];
-				
-				if (stat(path.c_str(), &buf))
-				{
-					return false;
-				}
-				
-				if (S_ISDIR(buf.st_mode))
-				{
-					std::cout << "(True)\n";
-					return true;
-				}
-				
-				std::cout << "(False)\n";
-				return false;
-			}
-			else
-			{
-				std::string path = ArgumentList[1];
-				
-				if (!stat(path.c_str(), &buf))
-				{
-					std::cout << "(True)\n";
-					return true;
-				}
-				
-				std::cout << "(False)\n";
-				return false;
-			}	
-		}
+		std::cout << "Shell: syntax error: test: missing path name\n";
+		return false;
+	}
+	else if (ArgumentList.size() == 2 && (ArgumentList[1] == "-e" || ArgumentList[1] == "-f" || ArgumentList[1] == "-d"))
+	{
+		std::cout << "Shell: syntax error: test: missing path name\n";
+		return false;
 	}
 	else
 	{
-		if (ArgumentList.size() == 2)
-		{
-			std::cout << "Shell: syntax error: test: missing path name\n";
-			return false;
-		}
-		else if (ArgumentList.size() == 3 && (ArgumentList[1] == "-e" || ArgumentList[1] == "-f" || ArgumentList[1] == "-d"))
-		{
-			std::cout << "Shell: syntax error: test: missing path name\n";
-			return false;			
-		}
 		if (ArgumentList[1] == "-e")
 		{
 			std::string path = ArgumentList[2];
@@ -229,6 +151,7 @@ bool CommandReceiver::Test(std::vector<std::string> &ArgumentList) const
 			
 			if (stat(path.c_str(), &buf))
 			{
+				std::cout << "(False)\n";
 				return false;
 			}
 			
@@ -247,23 +170,11 @@ bool CommandReceiver::Test(std::vector<std::string> &ArgumentList) const
 			
 			if (stat(path.c_str(), &buf))
 			{
+				std::cout << "(False)\n";
 				return false;
 			}
 			
 			if (S_ISDIR(buf.st_mode))
-			{
-				std::cout << "(True)\n";
-				return true;
-			}
-			
-			std::cout << "(False)\n";
-			return false;
-		}
-		else
-		{
-			std::string path = ArgumentList[1];
-			
-			if (!stat(path.c_str(), &buf))
 			{
 				std::cout << "(True)\n";
 				return true;
