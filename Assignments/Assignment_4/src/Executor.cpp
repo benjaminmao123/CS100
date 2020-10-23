c8d1b90af8881e6f599c6c22870cded959a985e8
benjaminmao123 <43651347+benjaminmao123@users.noreply.github.com>
2020-10-22 22:15:54 -0700

    update

--- Assignments/Assignment_4/src/Executor.cpp
+++ Assignments/Assignment_4/src/Executor.cpp
@@ -7,62 +7,62 @@
 
 bool Executor::operator()(const std::vector<Command*>& postfix)
 {
-	std::stack<Command*> result;
-	
-	for (const auto& t : postfix)
-	{
-		if (t)
-		{
-			const Connector* tempConn = dynamic_cast<Connector*>(t);
+    std::stack<Command*> result;
+    
+    for (const auto& t : postfix)
+    {
+        if (t)
+        {
+            const Connector* tempConn = dynamic_cast<Connector*>(t);
 
-			if (tempConn)
-			{
-				switch (t->Type())
-				{
-				case Token::TokenType::SEMICOLON:
-				case Token::TokenType::AND:
-				case Token::TokenType::OR:
-				case Token::TokenType::PIPE:
-				case Token::TokenType::INPUT_REDIRECTION:
-				case Token::TokenType::OUTPUT_REDIRECTION:
-				case Token::TokenType::OUTPUT_APPEND:
-				{
-					Command* rhs = result.top();
-					result.pop();
+            if (tempConn)
+            {
+                switch (t->Type())
+                {
+                case Token::TokenType::SEMICOLON:
+                case Token::TokenType::AND:
+                case Token::TokenType::OR:
+                case Token::TokenType::PIPE:
+                case Token::TokenType::INPUT_REDIRECTION:
+                case Token::TokenType::OUTPUT_REDIRECTION:
+                case Token::TokenType::OUTPUT_APPEND:
+                {
+                    Command* rhs = result.top();
+                    result.pop();
 
-					Command* lhs = result.top();
-					result.pop();
+                    Command* lhs = result.top();
+                    result.pop();
 
-					Command* conn = Token::Create(t->Type());
+                    Command* conn = Token::Create(t->Type());
 
-					conn->Left(lhs);
-					conn->Right(rhs);
-					
-					ptrs.emplace_back(conn);
-					result.push(conn);
-					break;
-				}
-				default:
-					break;
-				}
-			}
-			else
-				result.push(t);
-		}
-	}
+                    conn->Left(lhs);
+                    conn->Right(rhs);
+                    
+                    ptrs.emplace_back(conn);
+                    result.push(conn);
+                    break;
+                }
+                default:
+                    break;
+                }
+            }
+            else
+                result.push(t);
+        }
+    }
 
-	bool ret = false;
+    bool ret = false;
 
-	if (!result.empty() && result.top())
-	{
-                ret = result.top()->Execute();
-		
-		for (const auto& i : ptrs)
-			delete i;
-	}
+    if (!result.empty() && result.top())
+    {
+        ret = result.top()->Execute();
+        
+        for (const auto& i : ptrs)
+            delete i;
+    }
 
-	ptrs.clear();
-	std::stack<Command*>().swap(result);
+    ptrs.clear();
+    std::stack<Command*>().swap(result);
 
-	return ret;
+    return ret;
 }
